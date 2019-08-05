f2aad8aef6cb76c2d93ce0f35d53d199a7714798
60160158 <60160158@go.buu.ac.th>
2019-08-05 20:35:52 +0700

    funtion

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-if(true){
-let name = "pond"
-console.log(name)
-}
-let name = "kuy"
-console.log(name)
+// if(true){
+// let name = "pond"
+// console.log(name)
+// }
+// let name = "kuy"
+// console.log(name)
+ 
+
+
+function sum(x,y) { // regula
+     return x+y
+ }
+
+let result = function (x,y) { return x+y } //express
+
+let result_ar =  (x,y) => { return x+y } //arror funtion
+
+ 
+console.log(result_ar(3,4))
