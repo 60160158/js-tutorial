a0579590f570b4ec1021dc3f04875be16e28b81d
60160158 <60160158@go.buu.ac.th>
2019-08-05 21:57:26 +0700

    class

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-class pond{
-    constructor(money){
-        this.money = money
-    }
-    getVax(){
-        return this.money*(this.money/100*5)
-    }
-    static whoami(){
-        console.log("pond")
-    }
+// console.log('before')
+// setTimeout(() => {
+//     console.log("3sec")
+// }, 3000);
+// console.log("after")
+function a() {
+    console.log('before')
+
+} function b() {
+    setTimeout(() => {
+        console.log("3sec")
+    }, 3000);
+} function c() {
+    console.log("after")
+
+}
+callbackpond(a,b,c)
+function callbackpond(a, b, c) {
+    b()
+    a()
+    c()
 }
 
-let minipond= new pond(5)
-console.log(minipond.getVax())
 
-pond.whoami()
+
